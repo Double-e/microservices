8910671868b1fe4632c40e5d14e78c9915083960
Erikson Murrugarra <eriksonmurrugarra.dev@gmail.com>
2016-05-16 11:41:50 -0500

    removing unused declared packages in collections-core-microservice.

--- collections-core/src/main/java/com/doublee/collections/service/CollectionServiceImpl.java
+++ collections-core/src/main/java/com/doublee/collections/service/CollectionServiceImpl.java
@@ -3,7 +3,6 @@ package com.doublee.collections.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.doublee.collections.model.Collection;
